4b3b40617023ddb3d5047100f52e3fc7c6236cea
Niko Kommenda <nkommenda@hotmail.com>
2019-05-25 13:22:30 +0100

    up frequency

--- loop.sh
+++ loop.sh
@@ -3,7 +3,7 @@ while true; do
   npm run parse_continually
   npm run deploylive
 
-  time_diff=$(( 3600 - ( $SECONDS-$start_time ) ))
+  time_diff=$(( 900 - ( $SECONDS-$start_time ) ))
   echo "Time left after deploy: ${time_diff}s"
   sleep $(( time_diff > 0 ? $time_diff : 0 ))
 done
